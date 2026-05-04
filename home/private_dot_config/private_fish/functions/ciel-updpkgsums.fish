62d03845d36e2e51bd7a5abcc8c963455502300b
CAB233 <yidaduizuoye@outlook.com>
2026-05-04 21:07:05 +0800

    clean

--- home/private_dot_config/private_fish/functions/ciel-updpkgsums.fish
+++
@@ -1,9 +0,0 @@
-function ciel-updpkgsums
-    if test (count $argv) -ne 1
-        echo "Usage: ciel-updpkgsums <package-name>"
-        return 1
-    end
-
-    set package_name $argv[1]
-    sudo ciel shell -- acbs-build -gw $package_name
-end
