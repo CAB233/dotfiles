469f120dc31d64f3082db3805873e2dc2bfe5f64
CAB233 <yidaduizuoye@outlook.com>
2026-05-08 09:38:48 +0800

    Update .config/fish/functions/fish_prompt.fish

--- home/private_dot_config/private_fish/functions/fish_prompt.fish
+++ home/private_dot_config/private_fish/functions/fish_prompt.fish
@@ -1,33 +1,56 @@
-function _host
-    if set -q SSH_CONNECTION
-        echo -n (set_color --bold yellow)"(ssh)"
-        if functions -q fish_is_root_user; and fish_is_root_user
-            set_color --bold red
-        else
-            set_color --bold green
-        end
-        echo -n -s (prompt_login)' '
+# https://github.com/adamchristiansen/vertical-fish/blob/main/functions/__vertical_util_is_ssh.fish
+function _is_ssh
+    set --query SSH_CLIENT || set --query SSH_CONNECTION || set --query SSH_TTY
+end
+
+# https://github.com/adamchristiansen/vertical-fish/blob/main/functions/__vertical_util_is_git.fish
+function _is_git
+    if not type -q git
+        return 1
     end
+    command git rev-parse --git-dir > /dev/null 2> /dev/null
+end
+
+function _ssh
+    if _is_ssh
+        echo -n -s (set_color --bold yellow)"(ssh)"
+    end
+end
+
+function _login
+    if not _is_ssh
+        return
+    end
+
+    if fish_is_root_user
+        set_color --bold red
+    else
+        set_color --bold green
+    end
+    echo -n -s (prompt_login)' '
 end
 
 function _dir_status
     echo -n -s (set_color --bold cyan)(prompt_pwd)' '
 end
 
-function _git_status
-    if [ "$GIT_BRANCH" ]
-        set branch "$GIT_BRANCH"
-        echo -n -s (set_color normal)"[ " (set_color --bold magenta)$branch (set_color normal)" ] "
+function _git_branch
+    if not _is_git
+        return
     end
+
+    set --local branch (command git describe --tags --exact-match 2> /dev/null \
+        || command git symbolic-ref -q --short HEAD \
+        || command git rev-parse --short HEAD)
+    echo -n -s (set_color normal)"[ " (set_color --bold magenta)$branch (set_color normal)" ] "
 end
 
 function fish_prompt
-    set -l last_status $status
-    set -lx fish_prompt_pwd_dir_length 0
-    set -gx GIT_BRANCH (git symbolic-ref HEAD 2>/dev/null | sed -e 's|^refs/heads/||')
+    set --local last_status $status
+    set --local --export fish_prompt_pwd_dir_length 0
 
     echo
-    echo -n -s (_host) (_dir_status) (_git_status)
+    echo -n -s (_ssh) (_login) (_dir_status) (_git_branch)
     echo
     if test $last_status -eq 0
         echo -n (set_color green)'❯ '
